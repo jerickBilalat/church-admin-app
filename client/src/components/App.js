fbc69b23de8bcf40320fecf16582c724f5323868
Jerick Bilalat <jrk.mb.xm@gmail.com>
2018-01-15 20:56:19 -0600

    Add initial header, dashboard, eventNew, and landing components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>
+const EventNew = () => <h2>EventNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 
 const App = () => {
     return (
-        <div>
-            NLCF Admin
+        <div className="container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/events" component={Dashboard} />
+                    <Route path="/events/new" component={EventNew} />
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
