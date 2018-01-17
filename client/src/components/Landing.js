1781486ee87aa65067dc9274541dd731fd0864a0
Jerick Bilalat <jrk.mb.xm@gmail.com>
2018-01-16 19:37:16 -0600

    Add Landing Page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+    let style = {
+        'textAlign': 'center'
+    }
+    return (
+        <div style={style}>
+            <h1>NLCF Admin Landing Page</h1>
+            <p>Login with Google then create, edit, and publish events</p>
+        </div>
+    )
+}
+
+export default Landing;
