b9dde5ecaa25da98090204c2dfecfc8998ee4bbe
Jerick Bilalat <jrk.mb.xm@gmail.com>
2018-02-04 17:22:58 -0600

    add asyn await error handling in the action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,24 +3,32 @@ import { FETCH_USER, FETCH_EVENTS } from './types';
 
 
 export const fetchUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user');
-    dispatch({
-        type: FETCH_USER, 
-        payload: res.data
-    });
+    try {
+        const res = await axios.get('/api/current_user');
+        dispatch({ type: FETCH_USER, payload: res.data});
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 export const submitEvent = (values, history) => async dispatch => {
-    
-    const res = await axios.post('/api/events', values);
-
-    history.push('/');
-    dispatch({ type: 'submit_event', payload: res.data  });
-
+    try {
+        const res = await axios.post('/api/events', values);
+        history.push('/');
+        dispatch({ type: FETCH_USER, payload: res.data  });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 
 export const fetchEvents = () => async dispatch => {
-    const res = await axios.get('/api/events');
-    dispatch({ type: FETCH_EVENTS, payload: res.data});
+    try {
+        const res = await axios.get('/api/events');
+        dispatch({ type: FETCH_EVENTS, payload: res.data});
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
